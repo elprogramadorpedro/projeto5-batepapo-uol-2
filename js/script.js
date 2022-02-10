991b92660fe1c047322b46b69da851c5a6fa316c
celso-patiri <a16001@bcc.unifal-mg.edu.br>
2022-02-10 13:07:32 -0300

    Chat header/footer - Enter name event listener

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
-let loadHtmlFile = (href) => {
+const mainElement = document.querySelector('main') 
+mainElement.innerHTML=`${loadHtmlFile('../greeting.html')}`
+
+const nameInputEl = document.querySelector('#enter-name-input')
+
+//Add 'Enter' keypress event listener to enter name 'input'
+nameInputEl.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {                        // Number 13 is the "Enter" key on the keyboard          
+      event.preventDefault();                          // Cancel the default action, if needed       
+      document.querySelector("#input-button").click(); // Trigger the button element with a click
+    }
+});
+
+function loadHtmlFile(href){
     let xmlhttp = new XMLHttpRequest();
     xmlhttp.open("GET", href, false);
     xmlhttp.send();
@@ -6,17 +19,14 @@ let loadHtmlFile = (href) => {
 }
 
 let enterRoom = () => {
-    const nameInput = document.querySelector('#enter-name-input').value
-    const mainElement = document.querySelector('main') 
-
     let request = axios.post(
         'https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/participants',
-        {name: nameInput})
+        {name: nameInputEl.value})
 
     request.then(() => mainElement.innerHTML=`${loadHtmlFile('../chat.html')}`)
-    request.catch(() => {
+    request.catch((err) => {
         let errorMessage = document.querySelector('#error-message')
-        errorMessage.innerHTML = 'Esse nome ja esta em uso :('
+        errorMessage.innerHTML = 'Esse nome ja esta em uso :(<br> (ou input invalido)'
     })
 }
 
